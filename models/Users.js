d77106ee63a758e37245b80da6bb2387527a82b4
Efe Ariaroo <efeariaroo@gmail.com>
2016-06-10 19:38:35 +0100

    Can finally return the id of a newly inserted row on the usermaster table.

--- models/Users.js
+++ models/Users.js
@@ -6,4 +6,13 @@ function Users(tableName) {
 
 Users.prototype = dbObject;
 
+Users.prototype.insert = function(insertData, callbackOnInsertDone) {
+    dbObject.knex.insert(insertData)
+    .returning('id')
+    .into(this.tableName)
+    .then(function (id) {
+        callbackOnInsertDone(parseInt(id));
+    });
+}
+
 module.exports = new Users('usermaster');
--- spec/WatchWithSpec.js
+++ spec/WatchWithSpec.js
@@ -2,24 +2,22 @@ var Users = require('../models/Users');
 
 
 describe("Database tests", function() {
-  describe("> Users table tests", function() {
-    it("> User insert, retrieve and delete", function() {
-      Users.insert([{'email_address': "garfunkel@gmail.com"}], function() {
-        Users.findBy('email_address', "garfunkel@gmail.com", function(userResults) {
-          expect(userResults.length).toEqual(1);
-          console.log("Gotten userResults: " + JSON.stringify(userResults));
+    describe("> Users table tests", function() {
+        it("> User insert, retrieve and delete", function() {
+            Users.insert([{'email_address': "garfunkel@gmail.com"}], function(idOfNewUser) {
+                Users.findBy('id', idOfNewUser, function(userResults) {
+                    expect(userResults.length).toEqual(1);
+                    expect(userResults[0].email_address).toEqual("garfunkel@gmail.com");
 
-          expect(userResults[0].email_address).toEqual("garfunkel@gmail.com");
-
-          Users.deleteBy('id', userResults[0].id, function(deleteReturn) {
-            console.log("Delete returns: " + deleteReturn);
-          });
+                    Users.deleteBy('id', userResults[0].id, function(deleteReturn) {
+                        expect(deleteReturn).toEqual(1);
+                    });
+                });
+            });
         });
-      });
     });
-  });
 
-  //--More types of database tests
+    //--More types of database tests
 
 });
 
