bb7e0df81f13219d340d67f6952770930a06f995
Efe Ariaroo <efeariaroo@gmail.com>
2016-06-19 09:53:32 +0100

    Adds check in most recent migration file to see if the 'full_name' column exists before adding it to the 'social_identities' table

--- migrations/20160610222458_watchwithdb.js
+++ migrations/20160610222458_watchwithdb.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
     return Promise.all([
-        knex.schema.alterTable('social_identities', function(table) {
-            table.string('full_name');
+        knex.schema.hasColumn('social_identities', 'full_name').then(function(result) {
+            if(!result) {
+                knex.schema.alterTable('social_identities', function(table) {
+                    table.string('full_name')
+                })
+            } else {
+                console.log("full_name column already exists in social_identities table")
+            }
         })
     ])
 };
