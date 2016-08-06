71bfa9a54b45e3f062000eccdc79c8d6b5a460a2
Efe Ariaroo <efeariaroo@gmail.com>
2016-08-06 20:44:22 +0100

    Gives better names to db configuration variables in db.js

--- db.js
+++ db.js
@@ -1,14 +1,16 @@
-var config = require('./knexfile.js');
+var knexDbConfigs = require('./knexfile.js');
 
 var dbEnvVariable = 'WatchWith_DbEnv';
 
-if (process.env[dbEnvVariable] === undefined){
+if (process.env[dbEnvVariable] === undefined) {
     throw new Error('Environment variable for: ' + dbEnvVariable + ' must exist!');
 } else {
     var dbEnv = process.env[dbEnvVariable];
     console.log("WatchWith_DbEnv: " + dbEnv);
-    var knex = require('knex')(config[dbEnv]);
 
-    module.exports = knex;
-    knex.migrate.latest([config]);
+    var currentDbConfig = knexDbConfigs[dbEnv];
+    var watchWithDb = require('knex')(currentDbConfig);
+
+    module.exports = watchWithDb;
+    watchWithDb.migrate.latest([currentDbConfig]);
 }
